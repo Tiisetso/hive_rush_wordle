298a4514138bce12d97ce6e9aa9e38c13f9efc5f
nraatika <nraatika@student.hive.fi>
2025-06-27 14:04:08 +0300

    simple elimination added, doesn't act correctly with multiples

--- filter_dict.py
+++ filter_dict.py
@@ -12,7 +12,8 @@ def	filter_correct(pair, correct_list):
 			return 0
 	return 1
 
-def	filter_eliminated(key, elim_list):
+def	filter_eliminated(pair, elim_list):
+	key, value = pair
 	for i in elim_list:
 		if key.find(i) != -1:
 			return 0
@@ -26,15 +27,16 @@ with open('filtered.json') as full_file:
 	data = {"dict": dictionary, "correct": [], "eliminated": [], "incorrect": []}
 
 #	data['correct'].append({'o': 1})
-	data['correct'].append({'a': 2})
-	data['correct'].append({'e': 4})
+#	data['correct'].append({'a': 2})
+#	data['correct'].append({'e': 4})
 
-#	data['eliminated'] = ['o', 'r', 'a', 't', 'e']
+	data['eliminated'] = ['o', 'r', 'a', 't', 'e']
 	print("full data has ", len(data['dict']), " entries.")
 	print("\ncorrect letters and positions:", data['correct'])
 	print("\neliminated letters:", data['eliminated'])
 	print("\nincorrect positions for correct letters:", data['incorrect'])
 
 	correct = dict(filter(lambda x: filter_correct(x, data['correct']), data['dict'].items()))
+	correct = dict(filter(lambda x: filter_eliminated(x, data['eliminated']), data['dict'].items()))
 	print("\nfiltering for corrects has", len(correct), " entries:")
 	print(correct)
