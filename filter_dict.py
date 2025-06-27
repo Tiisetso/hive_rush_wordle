0200f3041477bd545e12e4d4771458334f9bdadb
nraatika <nraatika@student.hive.fi>
2025-06-27 15:40:49 +0300

    all filters implemented, doubles still uncertain

--- filter_dict.py
+++ filter_dict.py
@@ -12,31 +12,74 @@ def	filter_correct(pair, correct_list):
 			return 0
 	return 1
 
-def	filter_eliminated(pair, elim_list):
+def filter_incorrect_pos(pair, incorrect_list, min_chars):
 	key, value = pair
-	for i in elim_list:
-		if key.find(i) != -1:
+	for i in incorrect_list:
+#		print("\nitem:", i)
+		if not i:
+			continue
+		(char, position), = i.items()
+		if key.count(char) < min_chars[char]:
+			return 0
+		if key[position] == char:
 			return 0
 	return 1
 
+def	filter_eliminated(pair, max_chars):
+	key, value = pair
+	for i in max_chars.keys(): 
+		if key.count(i) > max_chars[i]:
+			return 0
+	return 1
 
+def update_correct(data, pair):
+	(key, value), = pair.items()
+	data['correct'].append(pair)
+	data['min_chars'][key] = data['min_chars'][key] + 1
 
+def update_eliminate(data, char):
+	data['max_chars'][char] = data['min_chars'][char]
+
+def update_incorrect(data, pair):
+	(key, value), = pair.items()
+	data['incorrect'].append(pair)
+	data['min_chars'][key] = data['min_chars'][key] + 1
+	
 with open('filtered.json') as full_file:
 	dictionary = json.load(full_file)
 	
-	data = {"dict": dictionary, "correct": [], "eliminated": [], "incorrect": []}
+	data = {"dict": dictionary, "correct": [], "incorrect": []
+	, "min_chars": {}, "max_chars": {}}
 
+	data['min_chars'] = {chr(i): 0 for i in range(ord('a'), ord('z') + 1)}
+	data['max_chars'] = {chr(i): 5 for i in range(ord('a'), ord('z') + 1)}
+	update_correct(data, {'o': 1})
+	update_eliminate(data, 't')
+	update_eliminate(data, 'r')
+	update_eliminate(data, 'a')
+	update_incorrect(data, {'e': 4})
+	
 #	data['correct'].append({'o': 1})
 #	data['correct'].append({'a': 2})
 #	data['correct'].append({'e': 4})
+#	data['eliminated'] = ['r', 't']
 
-	data['eliminated'] = ['o', 'r', 'a', 't', 'e']
 	print("full data has ", len(data['dict']), " entries.")
 	print("\ncorrect letters and positions:", data['correct'])
-	print("\neliminated letters:", data['eliminated'])
 	print("\nincorrect positions for correct letters:", data['incorrect'])
+	print("\nmin_char_count:", data['min_chars'])
+	print("\nmax_char_count:", data['max_chars'])
+
 
 	correct = dict(filter(lambda x: filter_correct(x, data['correct']), data['dict'].items()))
-	correct = dict(filter(lambda x: filter_eliminated(x, data['eliminated']), data['dict'].items()))
-	print("\nfiltering for corrects has", len(correct), " entries:")
+	updated = dict(filter(lambda x: filter_eliminated(x, data['max_chars']), correct.items()))
+	final = dict(filter(lambda x: filter_incorrect_pos(x, data['incorrect'], data['min_chars']), updated.items()))
+	
+	print("\nafter filtering for correct 'o' at index 1:", len(correct), " entries:")
 	print(correct)
+	print("\nafter filtering for eliminated 't', 'r', 'a':", len(updated), " entries:")
+	print(updated)
+	print("\nafter filtering for incorrect 'e' at index 4:", len(final), " entries:")
+	print(final)
+
+
