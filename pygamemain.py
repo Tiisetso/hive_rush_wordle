f84591179b4ba848377b7f53ac4846143741aff5
Cristian Davidov <cdavidov@c1r3p6.hive.fi>
2025-06-28 11:50:13 +0300

    fixed graphics

--- pygamemain.py
+++ pygamemain.py
@@ -11,7 +11,6 @@ screen = pygame.display.set_mode((1000, 1500))
 
 # set a background
 background = pygame.image.load("grid.png")
-exit = False
 
 # colours 
 white = (255, 255, 255)
@@ -19,50 +18,46 @@ green = (0, 200, 0)
 grey = (128, 128, 128)
 yellow = (255, 255, 0)
 black = (0, 0, 0)
-colour = white
-# positions
-X = 140
-Y = 250
-position1 = (0,0)
-position2 = (X, Y)
 
 # text details
 font = pygame.font.SysFont(None, 200)
-input_text = ''
-correct_letter = "A"
-correctish_letter = "Z"
-#letter_indent = letter_surface.get_rect(center = position2)
 
+user_input = ""
+correct_word = "WORDL"
 # input should be always active
-active = True
+running = True
 
-while True:
+while running:
+    screen.fill(white)
+    #screen.blit(background, (0, 0))
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
+            break
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
-                input_text = input_text[:-1]
-            else:
-                if len(input_text) < 1 and event.unicode.isalpha():
-                    input_text += event.unicode.upper()
-
-    
-    screen.fill(white)
-    screen.blit(background, dest = position1)
-    if input_text == correct_letter:
-        pygame.draw.rect(screen, green, pygame.Rect(56, 169, 158, 157))
-    elif input_text == correctish_letter: 
-        pygame.draw.rect(screen, yellow, pygame.Rect(56, 169, 158, 157))
-    pygame.draw.rect(screen, colour, pygame.Rect(239, 169, 158, 157))
-    pygame.draw.rect(screen, colour, pygame.Rect(421, 169, 158, 157))
-    pygame.draw.rect(screen, colour, pygame.Rect(604, 169, 158, 157))
-    pygame.draw.rect(screen, colour, pygame.Rect(786, 169, 158, 157))
-    #screen.blit(text_surface, dest = position2) 
-    text_surface = font.render(input_text, True, black)
-    text_rect = text_surface.get_rect(center = position2)
-    screen.blit(text_surface, text_rect)
+                user_input = user_input[:-1]
+            elif len(user_input) < 5 and event.unicode.isalpha():
+                    user_input += event.unicode.upper()
+    #screen.blit(background, (0, 0))
+    # Drwing the letters and highlight blocks
+    for i in range(len(user_input)):
+        letter = user_input[i]
+        x = 55 + i * 183
+        y = 170
+        color = grey
+        if i < len (correct_word):
+            if letter == correct_word[i]:
+                color = green
+            elif letter in correct_word:
+                color = yellow
+        pygame.draw.rect(screen, color, pygame.Rect(x, y, 156, 156))
+        text_surface = font.render(letter, True, black)
+        text_rect = text_surface.get_rect(center = (x + 79, y +78))
+        screen.blit(text_surface, text_rect)
+    screen.blit(background, (0, 0))
     pygame.display.update()
+pygame.quit()
+sys.exit()
+
