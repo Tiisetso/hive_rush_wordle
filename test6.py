7c4198135da4e9980cfedbf2c764dee6f396bf9c
Cristian Davidov <cdavidov@c1r3p6.hive.fi>
2025-06-28 08:10:02 +0300

    image and updated test6

--- test6.py
+++ test6.py
@@ -1,60 +1,64 @@
 import pygame
+import sys
 
 pygame.init()
 
+# name on the window
+pygame.display.set_caption('Hivedle!')
 
-# Define rgb values
+# set screen size
+screen = pygame.display.set_mode((1000, 1000))
 
-white = (255, 255, 255)
-green = (0, 255, 0)
-blue = (0, 0, 128)
-position = (0,0)
-position2 = (110,110)
-
-
-# assigning coordinates
-X = 700
-Y = 700
-
-# create the display surface object
-# of specific dimension..e(X, Y).
-display_surface = pygame.display.set_mode((X, Y))
-
-# set the pygame window name
-pygame.display.set_caption('Show Text')
-
-# create a font object.
-# 1st parameter is the font file
-# which is present in pygame.
-# 2nd parameter is size of the font
-font = pygame.font.Font('freesansbold.ttf', 32)
-
-# create a text surface object,
-# on which text is drawn on it.
-text = font.render('I am WORDLE', True, green, blue)
-
-textRect = text.get_rect()
-# set the center of the rectangular object.
-textRect.center = (X // 2, Y // 2)
-
-
-# CREATING CANVAS
-canvas = pygame.display.set_mode((1000,1000))
-
-# TITLE OF CANVAS
-pygame.display.set_caption("Show Image")
-
-image = pygame.image.load("hexagon.jpg")
+# set a background
+background = pygame.image.load("hexagons.png")
 exit = False
 
-#display_surface.blit(text, textRect)
-while not exit:
-    canvas.fill(white)
-    canvas.blit(image, dest = position)
-    canvas.blit(text, dest = position2)
+# colours 
+white = (255, 255, 255)
+green = (0, 200, 0)
+grey = (128, 128, 128)
+yellow = (255, 255, 0)
+black = (0, 0, 0)
+colour = white
+# positions
+X = 500
+Y = 500
+position1 = (0,0)
+position2 = (175,175)
 
+# text details
+font = pygame.font.SysFont(None, 200)
+input_text = ''
+# input should be always active
+active = True
+
+while True:
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
-            exit = True
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_BACKSPACE:
+                input_text = input_text[:-1]
+            else:
+                if len(input_text) < 5 and event.unicode.isalpha():
+                    input_text += event.unicode.upper()
+
+    
+    #screen.fill(white)
+    screen.blit(background, dest = position1)
+    text_surface = font.render(input_text, True, black)
+
+    pygame.draw.rect(screen, colour, pygame.Rect(100, 100, 150, 150))
+    pygame.draw.rect(screen, colour, pygame.Rect(270, 100, 150, 150))
+    pygame.draw.rect(screen, colour, pygame.Rect(440, 100, 150, 150))
+    pygame.draw.rect(screen, colour, pygame.Rect(610, 100, 150, 150))
+    pygame.draw.rect(screen, colour, pygame.Rect(780, 100, 150, 150))
+    screen.blit(text_surface, dest = position2)
+    letter_surface = font.render('W', True, green)
+    letter_rect = letter_surface.get_rect(center=position2)
+    screen.blit(letter_surface, letter_rect)
 
     pygame.display.update()
