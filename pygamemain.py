ae11d8b8f5e375a5c97b3d045a29f9426fd2ab38
Cristian Davidov <cdavidov@c1r3p6.hive.fi>
2025-06-28 09:52:02 +0300

    updated text and colours

--- pygamemain.py
+++ pygamemain.py
@@ -7,10 +7,10 @@ pygame.init()
 pygame.display.set_caption('Hivedle!')
 
 # set screen size
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((1000, 1500))
 
 # set a background
-background = pygame.image.load("hexagons.png")
+background = pygame.image.load("grid.png")
 exit = False
 
 # colours 
@@ -21,14 +21,18 @@ yellow = (255, 255, 0)
 black = (0, 0, 0)
 colour = white
 # positions
-X = 500
-Y = 500
+X = 140
+Y = 250
 position1 = (0,0)
-position2 = (175,175)
+position2 = (X, Y)
 
 # text details
 font = pygame.font.SysFont(None, 200)
 input_text = ''
+correct_letter = "A"
+correctish_letter = "Z"
+#letter_indent = letter_surface.get_rect(center = position2)
+
 # input should be always active
 active = True
 
@@ -43,22 +47,22 @@ while True:
             if event.key == pygame.K_BACKSPACE:
                 input_text = input_text[:-1]
             else:
-                if len(input_text) < 5 and event.unicode.isalpha():
+                if len(input_text) < 1 and event.unicode.isalpha():
                     input_text += event.unicode.upper()
 
     
-    #screen.fill(white)
+    screen.fill(white)
     screen.blit(background, dest = position1)
+    if input_text == correct_letter:
+        pygame.draw.rect(screen, green, pygame.Rect(56, 169, 158, 157))
+    elif input_text == correctish_letter: 
+        pygame.draw.rect(screen, yellow, pygame.Rect(56, 169, 158, 157))
+    pygame.draw.rect(screen, colour, pygame.Rect(239, 169, 158, 157))
+    pygame.draw.rect(screen, colour, pygame.Rect(421, 169, 158, 157))
+    pygame.draw.rect(screen, colour, pygame.Rect(604, 169, 158, 157))
+    pygame.draw.rect(screen, colour, pygame.Rect(786, 169, 158, 157))
+    #screen.blit(text_surface, dest = position2) 
     text_surface = font.render(input_text, True, black)
-
-    pygame.draw.rect(screen, colour, pygame.Rect(100, 100, 150, 150))
-    pygame.draw.rect(screen, colour, pygame.Rect(270, 100, 150, 150))
-    pygame.draw.rect(screen, colour, pygame.Rect(440, 100, 150, 150))
-    pygame.draw.rect(screen, colour, pygame.Rect(610, 100, 150, 150))
-    pygame.draw.rect(screen, colour, pygame.Rect(780, 100, 150, 150))
-    screen.blit(text_surface, dest = position2)
-    letter_surface = font.render('W', True, green)
-    letter_rect = letter_surface.get_rect(center=position2)
-    screen.blit(letter_surface, letter_rect)
-
+    text_rect = text_surface.get_rect(center = position2)
+    screen.blit(text_surface, text_rect)
     pygame.display.update()
